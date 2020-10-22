2540fd93d3c1997c12fd6e2025373e105da89257
ZyadYhia <zeyad.yhia95@gmail.com>
2020-10-23 01:21:26 +0200

    KEYPAD

--- Keypad/Atmel Studio/Keypad/main.c
+++ Keypad/Atmel Studio/Keypad/main.c
@@ -9,8 +9,12 @@ int main(void)
 	DDRA |= 0xFF;
 	while (1)
 	{
+		//RESET KEYPAD
 		PORTD |= (1<<0)|(1<<1)|(1<<2);
-		if (isPressedD(4)) //First H-Line
+		//---------------------------------------------------------------------------------------------------------------------
+		//--------------------------------------------------1'st H-Line--------------------------------------------------------
+		//---------------------------------------------------------------------------------------------------------------------
+		if (isPressedD(4))
 		{
 			setPinD(0);
 			resetPinD(1);
@@ -18,8 +22,8 @@ int main(void)
 			if (isPressedD(4))
 			{
 				//No.3
-				setPortA(0);
-				setPortA(3);
+				setPortA(0); //reset 7-SEGMENT
+				setPortA(3); //set 7-SEGMENT
 			}
 			_delay_ms(100);
 			setPinD(1);
@@ -28,8 +32,8 @@ int main(void)
 			if (isPressedD(4))
 			{
 				//No.2
-				setPortA(0);
-				setPortA(2);
+				setPortA(0); //reset 7-SEGMENT
+				setPortA(2); //set 7-SEGMENT
 			}
 			_delay_ms(100);
 			setPinD(2);
@@ -38,12 +42,16 @@ int main(void)
 			if (isPressedD(4))
 			{
 				//No.1
-				setPortA(0);
-				setPortA(1);
+				setPortA(0); //reset 7-SEGMENT
+				setPortA(1); //set 7-SEGMENT
 			}
 			_delay_ms(100);
 		}
-		if (isPressedD(5)) //second H-Line
+		//---------------------------------------------------------------------------------------------------------------------
+		//--------------------------------------------------2'nd H-Line--------------------------------------------------------
+		//---------------------------------------------------------------------------------------------------------------------
+
+		if (isPressedD(5))
 		{
 			setPinD(0);
 			resetPinD(1);
@@ -51,8 +59,8 @@ int main(void)
 			if (isPressedD(5))
 			{
 				//No.6
-				setPortA(0);
-				setPortA(6);
+				setPortA(0); //reset 7-SEGMENT
+				setPortA(6); //set 7-SEGMENT
 			}
 			_delay_ms(100);
 			setPinD(1);
@@ -61,8 +69,8 @@ int main(void)
 			if (isPressedD(5))
 			{
 				//No.5
-				setPortA(0);
-				setPortA(5);
+				setPortA(0); //reset 7-SEGMENT
+				setPortA(5); //set 7-SEGMENT
 			}
 			_delay_ms(100);
 			setPinD(2);
@@ -71,11 +79,14 @@ int main(void)
 			if (isPressedD(5))
 			{
 				//No.4
-				setPortA(0);
-				setPortA(4);
+				setPortA(0); //reset 7-SEGMENT
+				setPortA(4); //set 7-SEGMENT
 			}
 			_delay_ms(100);
 		}
+		//---------------------------------------------------------------------------------------------------------------------
+		//--------------------------------------------------3'rd H-Line--------------------------------------------------------
+		//---------------------------------------------------------------------------------------------------------------------
 		if (isPressedD(6)) //third H-Line
 		{
 			setPinD(0);
@@ -84,8 +95,8 @@ int main(void)
 			if (isPressedD(6))
 			{
 				//No.9
-				setPortA(0);
-				setPortA(9);
+				setPortA(0); //reset 7-SEGMENT
+				setPortA(9); //set 7-SEGMENT
 			}
 			_delay_ms(100);
 			setPinD(1);
@@ -94,8 +105,8 @@ int main(void)
 			if (isPressedD(6))
 			{
 				//No.8
-				setPortA(0);
-				setPortA(8);
+				setPortA(0); //reset 7-SEGMENT
+				setPortA(8); //set 7-SEGMENT
 			}
 			_delay_ms(100);
 			setPinD(2);
@@ -104,11 +115,15 @@ int main(void)
 			if (isPressedD(6))
 			{
 				//No.7
-				setPortA(0);
-				setPortA(7);
+				setPortA(0); //reset 7-SEGMENT
+				setPortA(7); //set 7-SEGMENT
 			}
 			_delay_ms(100);
 		}
+		//---------------------------------------------------------------------------------------------------------------------
+		//--------------------------------------------------4'th H-Line--------------------------------------------------------
+		//---------------------------------------------------------------------------------------------------------------------
+
 		if (isPressedD(7)) //forth H-Line
 		{
 			setPinD(0);
@@ -117,6 +132,7 @@ int main(void)
 			if (isPressedD(7))
 			{
 				//No.#
+				setPortA(0X0F); //F 7-SEGMENT
 			}
 			_delay_ms(100);
 			setPinD(1);
@@ -125,7 +141,7 @@ int main(void)
 			if (isPressedD(7))
 			{
 				//No.0
-				setPortA(0);
+				setPortA(0); //reset 7-SEGMENT
 			}
 			_delay_ms(100);
 			setPinD(2);
@@ -134,6 +150,7 @@ int main(void)
 			if (isPressedD(7))
 			{
 				//No.*
+				setPortA(0); //reset 7-SEGMENT
 			}
 			_delay_ms(100);
 		}
